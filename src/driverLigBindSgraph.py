1194cce9bf883a6c5bea0d7da9483d3f6654fd9d
eldadHaber@gmail.com <eldadHaber@gmail.com>
2021-06-18 11:39:16 -0700

    added functionality to utils

--- src/driverLigBindSgraph.py
+++ src/driverLigBindSgraph.py
@@ -17,8 +17,10 @@ from src import graphNet as GN
 
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-lig = torch.load('../../../data/mpro_docking_data/mpro_lig_select.pt')
-pro = torch.load('../../../data/mpro_docking_data/mpro_rec_select.pt')
+#lig = torch.load('../../../data/intmin_docking_data/1f02_subset_train.pt')
+#lig = torch.load('../../../data/mpro_docking_data/mpro_lig_select.pt')
+#pro = torch.load('../../../data/mpro_docking_data/mpro_rec_select.pt')
+lig = torch.load('../../../data/intmin_docking_data/1f02_train.pt')
 
 
 def getLigData(lig,IND):
@@ -86,67 +88,67 @@ def getLigData(lig,IND):
 #II, JJ, XN, XE, score, NL = getLigData(lig,[1])
 
 
-def getPocketData(P,IND):
-
-    #X      = lig['coords']
-    #atype  = lig['atom_types']
-    #charge = lig['charge']
-    #atomc  = lig['atom_connect']
-    #btype  = lig['bond_type']
-    #
-    n = len(IND)
-    II = torch.zeros(0)
-    JJ = torch.zeros(0)
-    XE = torch.zeros(0)
-    XN = torch.zeros(0,18)
-
-    score = torch.zeros(n)
-    NP    = torch.zeros(n, dtype=torch.long)
-    cnt = 0
-    for j in range(n):
-        i = IND[j]
-
-        #I = (lig['atom_connect'][i][:,0]-1).long()
-        #J = (lig['atom_connect'][i][:,1]-1).long()
-        #II = torch.cat((II,I+cnt))
-        #JJ = torch.cat((JJ,J+cnt))
-
-        X = P['coords'][i]
-        D = utils.distanceMap(X)
-        D = D / D.std()
-        D = torch.exp(-2 * D)
-
-        # Choose k=11 neiboughrs
-        nsparse = 32
-        vals, indices = torch.topk(D, k=min(nsparse, D.shape[0]), dim=1)
-        nd = D.shape[0]
-        I = torch.ger(torch.arange(nd), torch.ones(nsparse, dtype=torch.long))
-        I = I.view(-1)
-        J = indices.view(-1).type(torch.LongTensor)
-
-        II = torch.cat((II,I+cnt))
-        JJ = torch.cat((JJ,J+cnt))
-
-        xn = P['atom_types'][i]
-        XN = torch.cat((XN,xn),dim=0)
-        xe = D[I, J]
-        XE = torch.cat((XE,xe))
-
-        cn = P['charges'][i].unsqueeze(1)
-        xn = torch.cat((xn, 5*cn), dim=1)
-
-        NP[j] = xn.shape[0]
-        cnt = cnt + xn.shape[0]
-
-    XN = XN.t()
-
-    IJ1 = torch.cat((II, JJ)).unsqueeze(1)
-    IJ2 = torch.cat((JJ, II)).unsqueeze(1)
-    IJ = torch.cat((IJ1, IJ2), dim=1)
-    IJ = IJ.unique(dim=0)
-    II = IJ[:,0]
-    JJ = IJ[:,1]
-    return II.long(), JJ.long(), XN.unsqueeze(0), XE.unsqueeze(0).unsqueeze(0), NP
+# def getPocketData(P,IND):
+#
+#     #X      = lig['coords']
+#     #atype  = lig['atom_types']
+#     #charge = lig['charge']
+#     #atomc  = lig['atom_connect']
+#     #btype  = lig['bond_type']
+#     #
+#     n = len(IND)
+#     II = torch.zeros(0)
+#     JJ = torch.zeros(0)
+#     XE = torch.zeros(0)
+#     XN = torch.zeros(0,18)
+#
+#     score = torch.zeros(n)
+#     NP    = torch.zeros(n, dtype=torch.long)
+#     cnt = 0
+#     for j in range(n):
+#         i = IND[j]
+#
+#         #I = (lig['atom_connect'][i][:,0]-1).long()
+#         #J = (lig['atom_connect'][i][:,1]-1).long()
+#         #II = torch.cat((II,I+cnt))
+#         #JJ = torch.cat((JJ,J+cnt))
+#
+#         X = P['coords'][i]
+#         D = utils.distanceMap(X)
+#         D = D / D.std()
+#         D = torch.exp(-2 * D)
+#
+#         # Choose k=11 neiboughrs
+#         nsparse = 32
+#         vals, indices = torch.topk(D, k=min(nsparse, D.shape[0]), dim=1)
+#         nd = D.shape[0]
+#         I = torch.ger(torch.arange(nd), torch.ones(nsparse, dtype=torch.long))
+#         I = I.view(-1)
+#         J = indices.view(-1).type(torch.LongTensor)
+#
+#         II = torch.cat((II,I+cnt))
+#         JJ = torch.cat((JJ,J+cnt))
+#
+#         xn = P['atom_types'][i]
+#         XN = torch.cat((XN,xn),dim=0)
+#         xe = D[I, J]
+#         XE = torch.cat((XE,xe))
+#
+#         cn = P['charges'][i].unsqueeze(1)
+#         xn = torch.cat((xn, 5*cn), dim=1)
+#
+#         NP[j] = xn.shape[0]
+#         cnt = cnt + xn.shape[0]
+#
+#     XN = XN.t()
+#
+#     IJ1 = torch.cat((II, JJ)).unsqueeze(1)
+#     IJ2 = torch.cat((JJ, II)).unsqueeze(1)
+#     IJ = torch.cat((IJ1, IJ2), dim=1)
+#     IJ = IJ.unique(dim=0)
+#     II = IJ[:,0]
+#     JJ = IJ[:,1]
+#     return II.long(), JJ.long(), XN.unsqueeze(0), XE.unsqueeze(0).unsqueeze(0), NP
 
 #II, JJ, XN, XE, NP = getPocketData(pro,[1])
 
@@ -170,12 +172,13 @@ def computeScore(XNOutP, XNOutL, NP, NL, H=0):
 # Setup the network for ligand and its parameters
 
 nNin    = 19
-nopen   = 8
-nhid    = 8
+nopen   = 32
 nNclose = 8
 nlayer  = 12
 
 modelL = GN.graphNetwork(nNin, nopen, nNclose, nlayer)
+#modelL = GN.graphNetworkLin(nNin, nopen, nNclose, nlayer)
+
 modelL.to(device)
 
 
@@ -191,34 +194,28 @@ xnOutL = modelL(xnL, xeL, GL)
 
 # network for the protein
 # Setup the network for ligand and its parameters
-nNin = 18
-nopen = 8
-nhid  = 8
-nNclose = 8
-nlayer = 64
-
-modelP = GN.graphNetwork(nNin, nopen, nNclose, nlayer)
-modelP.to(device)
-
-total_params = sum(p.numel() for p in modelP.parameters())
-print('Number of parameters  for pocket', total_params)
-
-IP, JP, xnP, xeP, NP = getPocketData(pro,[0,1])
-nNodesP = xnP.shape[2]
-GP = GO.graph(IP, JP, nNodesP)
-xnOutP = modelP(xnP, xeP, GP)
-
-s = computeScore(xnOutP, xnOutL, NP, NL)
+#nNin = 18
+#nopen = 128
+#nhid  = 8
+#nNclose = 8
+#nlayer = 64
+#
+# modelP = GN.graphNetwork(nNin, nopen, nNclose, nlayer)
+# modelP.to(device)
+#
+# total_params = sum(p.numel() for p in modelP.parameters())
+# print('Number of parameters  for pocket', total_params)
+#
+# IP, JP, xnP, xeP, NP = getPocketData(pro,[0,1])
+# nNodesP = xnP.shape[2]
+# GP = GO.graph(IP, JP, nNodesP)
+# xnOutP = modelP(xnP, xeP, GP)
+#
+# s = computeScore(xnOutP, xnOutL, NP, NL)
 
 #### Start Training ####
-lrO = 1e-2
-lrC = 1e-2
-lrW = 1e-2
-lrE1 = 1e-2
-lrE2 = 1e-2
-lrH  = 1e-1
 
-H = nn.Parameter(torch.zeros(1))
+H = nn.Parameter(torch.ones(1)-6.6126)
 
 #optimizer = optim.Adam([{'params': modelP.K1Nopen, 'lr': lrO},
 #                        {'params': modelP.K2Nopen, 'lr': lrO},
@@ -233,55 +230,62 @@ H = nn.Parameter(torch.zeros(1))
 #                        {'params': modelL.KNclose, 'lr': lrC},
 #                        {'params': modelL.Kw, 'lr': lrW}])
 
+ndata = 194000
+batchSize = 1000
+f = 1. #batchSize/ndata
+lrO = 1e-2*f
+lrC = 1e-2*f
+lrW = 1e-2*f
+lrE1 = 1e-2*f
+lrE2 = 1e-2*f
+lrH  = 1e-1*f
+
 optimizer = optim.Adam([{'params': modelL.K1Nopen, 'lr': lrO},
-                        {'params': modelL.K2Nopen, 'lr': lrO},
-                        {'params': modelL.KE1, 'lr': lrE1},
+                       {'params': modelL.K2Nopen, 'lr': lrO},
+                       {'params': modelL.KE1, 'lr': lrE1},
                         {'params': modelL.KE2, 'lr': lrE2},
-                        {'params': modelL.KNclose, 'lr': lrC},
-                        {'params': modelL.Kw, 'lr': lrW},
-                        {'params': H, 'lr': lrH}])
+                       {'params': modelL.KNclose, 'lr': lrC},
+                       {'params': modelL.Kw, 'lr': lrW},
+                       {'params': H, 'lr': lrH}])
 
 
+#optimizer = optim.LBFGS([modelL.K1Nopen, modelL.K2Nopen, modelL.KE1, modelL.KE2, modelL.KNclose, modelL.Kw, H],
+#                        lr=1, max_iter=100)
 
 epochs = 500
 
-ndata = 900
 hist = torch.zeros(epochs)
 
 bestLoss = 1e11
-bestModelP = modelP
+#bestModelP = modelP
 bestModelL = modelL
 
 
-batchSize = 100
 for j in range(epochs):
     # Prepare the data
     aloss = 0.0
     for i in range(ndata//batchSize):
 
-        IND = torch.arange(i*batchSize,(i+1)*batchSize)
+#        def closure():
+        optimizer.zero_grad()
+        IND = torch.arange(i * batchSize, (i + 1) * batchSize)
         # Get the lig data
-        IL, JL, xnL, xeL, truescore,  NL = getLigData(lig, IND)
+        IL, JL, xnL, xeL, truescore, NL = getLigData(lig, IND)
         nNodesL = xnL.shape[2]
         GL = GO.graph(IL, JL, nNodesL)
-        # Get the pro data
-        #IP, JP, xnP, xeP, NP = getPocketData(pro, IND)
-        #nNodesP = xnP.shape[2]
-        #GP = GO.graph(IP, JP, nNodesP)
 
         optimizer.zero_grad()
         xnOutL = modelL(xnL, xeL, GL)
-        #xnOutP = modelP(xnP, xeP, GP)
-
-        #predScore = torch.dot(torch.mean(xnOutP, dim=2).squeeze(), torch.mean(xnOutL, dim=2).squeeze())
-        xnOutP    = torch.ones(xnOutL.shape)
-        #predScore = computeScore(xnOutP, xnOutL, NP, NL)
+        xnOutP = torch.ones(xnOutL.shape)
         predScore = computeScore(xnOutP, xnOutL, NL, NL, H)
 
-        loss = F.mse_loss(predScore, truescore)/F.mse_loss(truescore*0, truescore)
-
-        optimizer.zero_grad()
+        loss = F.mse_loss(predScore, truescore)  # /F.mse_loss(truescore*0, truescore)
         loss.backward()
+#            print(loss.item())
+#            return loss
+
+        #optimizer.step(closure)
+        optimizer.step()
 
         gC  = modelL.KNclose.grad.norm().item()
         gE1 = modelL.KE1.grad.norm().item()
@@ -290,9 +294,9 @@ for j in range(epochs):
         gO2 = modelL.K2Nopen.grad.norm().item()
         gw  = modelL.Kw.grad.norm().item()
 
+        #aloss = closure()
 
         aloss += loss.detach()
-        optimizer.step()
         # scheduler.step()
         nprnt = 1
         if (i + 1) % nprnt == 0:
@@ -302,9 +306,27 @@ for j in range(epochs):
             aloss = 0.0
 
             if aloss < bestLoss:
-                bestModelP = modelP
+                #bestModelP = modelP
                 bestModelL = modelL
                 bestLoss   = aloss
 
-plt.plot(predScore.detach())
-plt.plot(truescore.detach())
+
+IND = torch.arange(ndata)
+# Get the lig data
+IL, JL, xnL, xeL, truescore, NL = getLigData(lig, IND)
+nNodesL = xnL.shape[2]
+GL = GO.graph(IL, JL, nNodesL)
+
+xnOutL = modelL(xnL, xeL, GL)
+xnOutP = torch.ones(xnOutL.shape)
+predScore = computeScore(xnOutP, xnOutL, NL, NL, H)
+
+loss = F.mse_loss(predScore, truescore)  # /F.mse_loss(truescore*0, truescore)
+
+tss, ind = torch.sort(truescore)
+plt.plot(predScore[ind].detach())
+plt.plot(tss.detach())
+
+#tss, ind = torch.sort(predScore)
+#plt.plot(truescore[ind].detach())
+#plt.plot(tss.detach())
--- src/graphNet.py
+++ src/graphNet.py
@@ -119,6 +119,83 @@ class graphNetwork(nn.Module):
             xnold = tmp
 
         xn = F.conv1d(xn, self.KNclose.unsqueeze(-1))
-        xn = torch.cat((torch.relu(xn),torch.relu(-xn)),dim=1)
+        #xn = torch.cat((torch.relu(xn),torch.relu(-xn)),dim=1)
+
+        return xn
+
+
+class graphNetworkLin(nn.Module):
+
+    def __init__(self, nNin, nopen, nNclose, nlayer, h=0.1):
+        super(graphNetworkLin, self).__init__()
+
+        self.h = h
+        stdv = 1.0 #1e-2
+        stdvp = 1.0 # 1e-3
+        self.K1Nopen = nn.Parameter(torch.randn(nopen, nNin) * stdv)
+        self.K2Nopen = nn.Parameter(torch.randn(nopen, nopen) * stdv)
+
+        #nopen      = 3*nopen  # [xn; Av*xe; Div*xe]
+        self.nopen = nopen
+
+        nhid = 2*nopen
+        Id  = (torch.eye(nhid,2*nopen)).unsqueeze(0) #+ 1e-3*torch.randn(nhid,5*nopen)).unsqueeze(0)
+        Idt = (torch.eye(2*nopen, nhid)).unsqueeze(0) # + 1e-3*torch.randn(5*nopen,nhid)).unsqueeze(0)
+
+        IdTensor  = torch.repeat_interleave(Id, nlayer, dim=0)
+        IdTensort = torch.repeat_interleave(Idt, nlayer, dim=0)
+        self.KE1 = nn.Parameter(IdTensor * stdvp)
+        self.KE2 = nn.Parameter(IdTensort * stdvp)
+
+        self.KNclose = nn.Parameter(torch.randn(nNclose, nopen)*1e-2)
+        self.Kw = nn.Parameter(torch.ones(nopen,1))
+
+    def doubleLayer(self, x, K1, K2):
+
+        #x = torch.tanh(x)
+        x = F.conv1d(x, K1.unsqueeze(-1))  # self.edgeConv(x, K1)
+        #x = tv_norm(x)
+        x = torch.tanh(x)
+        x = F.conv1d(x, K2.unsqueeze(-1))
+        #x = torch.tanh(x)
+
+        return x
+
+    def forward(self, xn, xe, Graph):
+
+        # Opening layer
+        # xn = [B, C, N]
+        # xe =  [B, C, E]
+        # Opening layer
+        xn = self.doubleLayer(xn, self.K1Nopen, self.K2Nopen)
+        #xe = self.doubleLayer(xe, self.K1Eopen, self.K2Eopen)
+        #xn = torch.cat([xn,Graph.edgeDiv(xe), Graph.edgeAve(xe)], dim=1)
+
+        nlayers = self.KE1.shape[0]
+
+        xnold = xn
+        for i in range(nlayers):
+
+            # Compute the distance in real space
+            #x3    = F.conv1d(xn, self.KNclose.unsqueeze(-1))
+            w     = Graph.edgeLength(xn)
+            w     = self.Kw@w
+            w     = w/(torch.std(w)+1e-4)
+            w     = torch.exp(-(w**2))
+            #w     = torch.ones(xe.shape[2], device=xe.device)
+
+            gradX   = Graph.nodeGrad(xn,w)
+            intX    = Graph.nodeAve(xn,w)
+
+            dxe = torch.cat([gradX, intX], dim=1)
+
+            dxe  = self.doubleLayer(dxe, self.KE1[i], self.KE2[i])
+
+            divE = Graph.edgeDiv(dxe[:,:self.nopen,:],w)
+            aveE = Graph.edgeAve(dxe[:,self.nopen:2*self.nopen,:],w)
+
+            xn   = xn - self.h * (divE + aveE)
+
+        xn = F.conv1d(xn, self.KNclose.unsqueeze(-1))
 
         return xn
